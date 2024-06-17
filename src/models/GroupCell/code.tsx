a62412df6a05f400143d5c0db0c4f3db23f90869
eftsoons <svetafedorowitch@yandex.ru>
2024-06-17 21:19:10 +0300

    update

--- src/models/GroupCell/code.tsx
+++ src/models/GroupCell/code.tsx
@@ -4,9 +4,9 @@ import style from "./style.module.css"
 
 import {Node} from "../../Props"
 
-const GroupCell = ({children, height, key} : Node) => {
+const GroupCell = ({children, height} : Node) => {
     return (
-        <div key={key} style={{height: height}} className={style.groupmain}>
+        <div style={{height: height}} className={style.groupmain}>
             {Children.toArray(children).map((children) => (
             <Fragment>
                 {/* {index != 0 && <div className={style.seperator} />} */}
